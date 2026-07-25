cb8496f56e65f2d6d45c334e4f39738cdccaf339
luizgustavolc1-source <luizgustavo.lc1@gmail.com>
2026-07-25 10:03:52 +1000

    feat: add private coach workspace foundation

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
-
 import App from "./App";
+import { SessionProvider } from "./services/session";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <Toaster richColors position="top-right" />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><BrowserRouter><SessionProvider><App /></SessionProvider></BrowserRouter></React.StrictMode>);
